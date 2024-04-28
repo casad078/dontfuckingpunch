ccd5128efc595a8bfe336a7f35673b050b04e82a
Dani C <143529026+casad078@users.noreply.github.com>
2024-04-27 22:26:25 -0400

    last

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const gif = document.getElementById('gif');
+    const scrollButton1 = document.getElementById('why-it-started');
     const scrollButton = document.getElementById('scrollButton');
+    const scrollButton2 = document.getElementById('what-we-do');
+    const scrollButton3 = document.getElementById('taking-action');
+    const scrollButton4 = document.getElementById('contact');
 
-    // Play GIF when the page loads
-    gif.src = "gif.gif";
-
-    // Scroll to next section when the button is clicked
-    scrollButton.addEventListener('click', function() {
-        const nextSection = document.querySelector('.section-two');
-        nextSection.scrollIntoView({ behavior: 'smooth' });
+    scrollButton1.addEventListener('click', function() {
+        const nextSection1 = document.querySelector('.section-two');
+        scrollToSection(nextSection1);
     });
+
+    scrollButton.addEventListener('click', function() {
+        const nextSection1 = document.querySelector('.together');
+        scrollToSection(nextSection1);
+    });
+
+    scrollButton2.addEventListener('click', function() {
+        const nextSection2 = document.querySelector('.section-three');
+        scrollToSection(nextSection2);
+    });
+
+    scrollButton3.addEventListener('click', function() {
+        const nextSection3 = document.querySelector('.section-four');
+        scrollToSection(nextSection3);
+    });
+
+    scrollButton4.addEventListener('click', function() {
+        const nextSection3 = document.querySelector('.section-five');
+        scrollToSection(nextSection3);
+    });
+
+    function scrollToSection(section) {
+        const stickyMenu = document.getElementById('sticky-menu');
+        const stickyMenuHeight = stickyMenu.offsetHeight;
+        const bounding = section.getBoundingClientRect();
+        const offsetTop = window.pageYOffset + bounding.top - stickyMenuHeight;
+
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth'
+        });
+    }
 });
